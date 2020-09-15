27db66c149fddb15f94abd87833cbcec6e4fa545
dhakalsamundra <seapush02@gmail.com>
2020-09-15 20:58:11 +0300

    --no-verify

--- Database/db.js
+++ Database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('config');
+const config = require('./node_modules/config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
