035ebfba0858ddcba6bc1b95bbe60038abb562d3
dhakalsamundra <seapush02@gmail.com>
2020-09-20 19:04:30 +0300

    finalized the main file

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-const expresss = require('express');
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3001
+// Connect Database
+connectDB();
 
-app.listen(PORT, ()=> console.log(`Server is running in port ${PORRT}`))
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
