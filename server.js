6aafa002d3ded69c265b575d3c45eb3f5a703c51
dhakalsamundra <seapush02@gmail.com>
2020-09-22 22:53:48 +0300

    refactor the code

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
+import express from 'express'
+import mongoose from 'mongoose'
+import path from 'path'
+import apiContentType from './middleware/apiContentType'
+import authJwt from './middleware/auth'
+import { MONGODB_URI } from './util/secrets'
+import authRouter from './routers/auth'
+import userRouter from './routers/user'
+import contactRouter from './routers/contact'
+import unless from './util/unless'
 
-const app = express();
 
-// Connect Database
-connectDB();
+const app = express()
 
-// Init Middleware
+
+const mongoUrl = MONGODB_URI
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected...');
+})
+  .catch((err) => {
+    console.log(
+      'MongoDB connection error. Please make sure MongoDB is running. ' + err
+    )
+    process.exit(1)
+  })
+  
+//  const excludedPaths = [ /users/]
+
+// middlewares
 app.use(express.json({ extended: false }));
+// app.use('/api',apiContentType, unless(excludedPaths, authJwt))
 
-// Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+// use router
+app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/contacts', contactRouter)
 
-// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
@@ -24,6 +50,10 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
+
+// const PORT =  process.env.PORT || 3002
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`),
+// console.log('  Press CTRL-C to stop\n'));
 
 const PORT = process.env.PORT || 3002;
 
