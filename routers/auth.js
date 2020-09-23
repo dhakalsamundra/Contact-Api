50ddb343aca7ceb3658d113ef7f508b01a8ed6b0
dhakalsamundra <seapush02@gmail.com>
2020-09-23 18:18:33 +0300

    auth route

---
+++ routers/auth.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { check} from 'express-validator'
+import authJwt from '../middleware/auth'
+import
+ { findUserById, signIn, passwordRequestReset,resetPasswordTokenStatus,resetPassword} 
+ from '../controllers/auth'
+
+const router = express.Router()
+
+router.get('/', authJwt, findUserById)
+router.post('/',
+[
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+signIn)
+router.post('/forgetPassword', passwordRequestReset)
+router.get('/resetPasswordRequest/:token', resetPasswordTokenStatus)
+router.post('/resetPasswordRequest/:token', resetPassword)
+
+export default router
