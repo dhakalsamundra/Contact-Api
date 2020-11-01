574dfe4f2c03a63de108d40958eba809fa5817cb
dhakalsamundra <seapush02@gmail.com>
2020-11-01 14:36:35 +0200

    add route page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,10 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home';
-import ResetLink from './components/pages/ResetLink';
-import ResetPassword from './components/pages/NewPassword';
-import Register from './components/auth/Register';
-import Login from './components/auth/LogIn';
 import Alerts from './components/layout/Alerts';
-import PrivateRoute from './components/route/PrivateRoute';
-
 import ContactState from './context/contact/action';
 import AuthState from './context/auth/action';
 import AlertState from './context/alert/action';
+import Routes from './Routes'
 import './App.css';
 
 const App = () => {
@@ -19,21 +12,13 @@ const App = () => {
     <AuthState>
       <ContactState>
         <AlertState>
-          <Router>
             <Fragment>
               <Navbar />
               <div className='container'>
                 <Alerts />
-                <Switch>
-                  <PrivateRoute exact path='/dashboard' component={Home} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/' component={Login} />
-                  <Route exact path="/forgetPassword" component={ResetLink} />
-                  <Route exact path="/forgetPassword/:token" component={ResetPassword} />
-                </Switch>
+                <Routes />
               </div>
             </Fragment>
-          </Router>
         </AlertState>
       </ContactState>
     </AuthState>
