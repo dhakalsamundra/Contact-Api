c57c2d25cf8797d4642ab83067f541c4193a5181
dhakalsamundra <seapush02@gmail.com>
2020-11-01 14:38:09 +0200

    wrap app with router

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Index = () => (
+    <Router>
+        <App />
+    </Router>
+)
+ReactDOM.render(<Index />, document.getElementById('root'));
+
